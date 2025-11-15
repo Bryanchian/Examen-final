ce847212f9893dc306bc200362a11d90cd78f454
Bryanchian <bryanjosue2004@gmail.com>
2025-11-15 17:17:47 -0600

    Update e2.c

--- src/ejercicios-C/e2.c
+++ src/ejercicios-C/e2.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char name[30];
+    char name[128];
 
-    //tu codigo aqui ...
-    
+    printf("Ejercicio 2\n");
+    printf("Ingrese un nombre\n");
+    if (fgets(name, sizeof(name), stdin) != NULL) {
+        /* quitar salto de linea final si existe */
+        size_t n = strlen(name);
+        if (n > 0 && name[n-1] == '\n') name[n-1] = '\0';
+        printf("Hola %s\n", name);
+    } else {
+        printf("No se leyó nombre\n");
+    }
+
     return 0;
 }
