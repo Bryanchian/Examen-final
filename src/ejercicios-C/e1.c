45b98067662b3ebdf05da2eaba513feba39b8152
Bryanchian <bryanjosue2004@gmail.com>
2025-11-15 17:05:27 -0600

    Update e1.c

--- src/ejercicios-C/e1.c
+++ src/ejercicios-C/e1.c
@@ -4,7 +4,12 @@ int main() {
     int val;
 
     printf("Ejercicio 1\n");
-    //tu codigo aqui....
+    printf("Ingrese un valor entero\n");
+    if (scanf("%d", &val) == 1) {
+        printf("Salida: %d\n", val);
+    } else {
+        printf("Entrada no valida\n");
+    }
 
     return 0;
 }
